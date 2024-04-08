5e020a3f747b6aa41d6edeab5d14789c87a635eb
MycoG <154273635+MycoG@users.noreply.github.com>
2024-04-07 18:19:44 -0700

    Finished csv reading function

--- reads.py
+++ reads.py
@@ -15,32 +15,38 @@ class Location:
         self.name = name
         self.attributes = attributes
 
-
 user : Person
 locationList = []
+
+#[name, ext1, ext2, ext3, ext4, ext5, attr1, attr2, attr3]
     
 def load_locations_from_csv(csvfile):
     locations = []
-    csv_df = pd.read_csv(csvfile)
-    for x in range(0, len(csv_df)):
-        row = csv_df.iloc[x].tolist()
-        name = row[0]
-        attributes = row[1:6]
-        locations.append(Location(name, attributes))
+    with open(csvfile, "r") as f:
+        for line in f.readlines()[1:]:
+            line = line.strip().split(",")
+            name = line[0]
+            exts = line[1:6]
+            attrs = line[6:9]
+            locations.append(Location(name, attrs))
     return locations
 
 def load_user_from_csv(csvfile):
-    csv_df = pd.read_csv(csvfile)
-    for x in range(0, len(csvfile)):
-        row = csv_df.iloc[x].tolist()
-        name = row[0]
-        preferences = row[1:6]
-        return Person(name, preferences)
+    with open(csvfile, "r") as f:
+        line = f.readline()
+        line = line.strip().split(",")
+        name = line[0]
+        attrs = line[1:4]
+        return Person(name, attrs)
 
-def save_locations_from_csv(csvfile: str):
-    
-    return
+def save_locations_to_csv(locations, csvfile: str):
+    with open(csvfile, "w") as f:
+        for loc in locations:
+            f.write(",".join([loc.name] + [str(a) for a in loc.attributes])+"\n")
 
+def save_person_to_csv(person, csvfile: str):
+    with open(csvfile, "w") as f:
+        f.write(",".join([person.name] + [str(a) for a in person.attributes])+"\n")
 
 def main():
 
@@ -49,10 +55,11 @@ def main():
     
     loc = load_locations_from_csv("locations.csv")
     user = load_user_from_csv("user.csv")
+    print(loc[0].name)
+    print(loc[0].attributes)
     print(user.name)
+    print(user.preferences)
 
-
-    
 
 if __name__ == "__main__":
     main()
